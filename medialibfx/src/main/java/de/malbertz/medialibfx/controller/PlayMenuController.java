c1ac6fa9bc1b6ffe2b462937db06fea4bda01bfb
Prioe <michael.albertz@web.de>
2015-07-18 12:11:34 +0200

    Preparation for PlayMenu

--- medialibfx/src/main/java/de/malbertz/medialibfx/controller/PlayMenuController.java
+++ medialibfx/src/main/java/de/malbertz/medialibfx/controller/PlayMenuController.java
@@ -1,5 +1,28 @@
 package de.malbertz.medialibfx.controller;
 
-public class PlayMenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class PlayMenuController implements Initializable {
+
+  @FXML private Button nextButton;
+  @FXML private Button prevButton;
+  @FXML private Button playButton;
+  @FXML private ImageView coverImageView;
+  @FXML private Label durationLabel;
+  @FXML private Label status1Label;
+  @FXML private Label status2Label;
+  
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    // TODO Auto-generated method stub
+    
+  }
 
 }
